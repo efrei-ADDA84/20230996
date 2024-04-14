7fc326c0e33134454addc5f92454f6fbf963841b
imaneid77 <149710449+imaneid77@users.noreply.github.com>
2024-04-14 22:21:59 +0200

    commit

---
+++ TP2/TP2.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+import os
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_weather():
+    # Récupère la latitude et la longitude depuis la requête HTTP
+    latitude = request.args.get('lat')
+    longitude = request.args.get('lon')
+    api_key = os.environ.get('API_KEY')
+
+    # Vérifie si les paramètres et la clé API sont présents
+    if not latitude or not longitude:
+        return jsonify({'error': 'Latitude ou longitude non spécifiée.'}), 400
+    elif not api_key:
+        return jsonify({'error': 'Aucune clé API'}), 400
+
+    
+    api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+    response = requests.get(api_url)
+    
+    
+    if response.status_code == 200:
+        data = response.json()
+        weather = data.get('weather', [])
+        main = data.get('main')
+        if weather and main:
+            # Récupère la description météo et la température
+            description = weather[0].get('description')
+            temperature = main.get('temp')
+            # Renvoie les données en tant que JSON
+            return jsonify({
+                'description': description,
+                'temperature': temperature
+            })
+        else:
+            return jsonify({'error': 'Aucune donnée météo disponible.'}), 404
+    else:
+        # Renvoie une erreur si l'API OpenWeatherMap ne répond pas correctement
+        return jsonify({'error': 'Erreur lors de la requête à l\'API OpenWeather'}), response.status_code
+
+# Démarre le serveur pour écouter sur le port 8081
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8081)
