dc784d3927618847bbd480e5079ca5a0a1cbbd3a
imaneid77 <149710449+imaneid77@users.noreply.github.com>
2024-04-22 19:10:38 +0200

    v

--- TP4/variables.tf
+++ TP4/variables.tf
@@ -23,17 +23,17 @@ variable "username" {
   default     = "devops"
 }
 
-variable "vnet_name"{
-  type=string
-  default="network-tp4"
+variable "vnet_name" {
+  type    = string
+  default = "network-tp4"
 }
 
-variable "suscription_id"{
-  type=string
-  default="765266c6-9a23-4638-af32-dd1e32613047"
+variable "suscription_id" {
+  type    = string
+  default = "765266c6-9a23-4638-af32-dd1e32613047"
 }
 
-variable "subnet_name"{
-  type=string
-  default="internal"
+variable "subnet_name" {
+  type    = string
+  default = "internal"
 }
